95eb09dc6fcd2e750f5c677ff51b1f6619d3cbb2
dioh0922 <diohound@gmail.com>
2021-08-29 10:23:09 +0900

    import test

--- debug/test.py
+++ debug/test.py
@@ -6,8 +6,8 @@ import sys
 import io
 import os
 
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, "UTF-8")
-train_file = "./sonshi_jpn_pick.txt"
+#sys.stdout = io.TextIOWrapper(sys.stdout.buffer, "UTF-8")
+#train_file = "./sonshi_jpn_pick.txt"
 
 #model = sentence_module.generate_model(train_file)
 #print(model.make_sentence().replace(" ", ""))
