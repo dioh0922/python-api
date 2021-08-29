90a0232585bb0e9cdbc437f67bf90be09a273087
dioh0922 <diohound@gmail.com>
2021-08-29 10:01:30 +0900

    php try-catch

--- markovify/generate_sentence_api.php
+++ markovify/generate_sentence_api.php
@@ -1,19 +1,23 @@
 <?php
 ini_set( 'display_errors', 1 );
 	header('Content-Type: text/html; charset=UTF-8');
-	$full = "";
-	if(isset($_POST["lang"])){
-		if($_POST["lang"] != ""){
-			if($_POST["lang"] == "jpn"){
-				$full = "python generate_sentence.py ./sonshi_jpn_pick.txt";
-			}else if($_POST["lang"] == "chn"){
-				$full = "python generate_sentence.py ./sonshi_pick.txt";
-			}
-			exec($full, $out);
+	try{
+		$full = "";
+		if(isset($_POST["lang"])){
+			if($_POST["lang"] != ""){
+				if($_POST["lang"] == "jpn"){
+					$full = "python generate_sentence.py ./sonshi_jpn_pick.txt";
+				}else if($_POST["lang"] == "chn"){
+					$full = "python generate_sentence.py ./sonshi_pick.txt";
+				}
+				exec($full, $out);
 
-			foreach($out as $iter){
-				echo $iter;
+				foreach($out as $iter){
+					echo $iter;
+				}
 			}
 		}
+	}catch(Exception $e){
+		echo $e->getMessage();
 	}
 ?>
