1f1a34adf4d7d9f81dd2b5c6edb7261189605b46
dioh0922 <diohound@gmail.com>
2021-09-23 09:39:24 +0900

    test directory

---
+++ test.php
@@ -0,0 +1,52 @@
+<?php
+
+echo "hello world<br>";
+echo date("Y-m-d H:i");
+echo "<br>";
+
+/*
+exec("pip freeze", $out);
+
+foreach($out as $iter){
+	echo $iter;
+}
+*/
+
+echo "env<br>";
+echo getenv("TESSDATA_PREFIX");
+
+echo "<br>which<br>";
+$out = [];
+exec("which tesseract", $out);
+
+foreach($out as $iter){
+	echo $iter;
+}
+
+echo "<br>env<br>";
+$out = [];
+exec("printenv", $out);
+
+foreach($out as $iter){
+	echo $iter;
+}
+
+
+echo "<br>-v<br>";
+$out = [];
+exec("tesseract -v", $out);
+
+foreach($out as $iter){
+	echo $iter;
+}
+
+
+$out = [];
+exec("python call_ocr.py test.jpg", $out);
+
+foreach($out as $iter){
+	echo $iter;
+}
+
+
+?>
