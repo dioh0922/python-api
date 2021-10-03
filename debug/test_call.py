cf83d5c6a8c94fbd77cd60448598543dd03347e6
dioh0922 <diohound@gmail.com>
2021-10-03 10:09:33 +0900

    test python print

---
+++ debug/test_call.py
@@ -0,0 +1,22 @@
+import numpy as np
+import chainer
+import glob
+import random
+import matplotlib.pyplot as plt
+from PIL import Image
+from chainer import cuda, Function, \
+	report, training, utils, Variable
+from chainer import datasets, iterators, optimizers
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+import chainer.serializers as S
+from chainer.training import extensions
+from chainer.datasets import LabeledImageDataset
+from chainer.datasets import TransformDataset
+from chainer.training import extensions
+from chainer.datasets import tuple_dataset
+import datetime
+
+
+print("python start")
