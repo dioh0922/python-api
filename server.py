cc56d46a3f9b6a13bf496897da8be587dc459e3a
dioh0922 <diohound@gmail.com>
2023-02-11 21:07:25 +0900

    Render.com用の準備

--- server.py
+++ server.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 from markovify_module import sentence_module
 
 app = Flask(__name__)
 
-@app.route("/generate_sentence_api", methods=["POST"])
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/markovify")
+def markovify():
+	return render_template("markovify.html");
+
+@app.route("/markovify/generate_sentence_api", methods=["POST"])
 def generate_api():
 	model = sentence_module.generate_model(request.values.get("lang"))
 	return model.make_sentence().replace(" ", "")
