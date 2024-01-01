9a3bb68f0c1d81cd24d53c74219cbad73d904d18
dioh0922 <diohound@gmail.com>
2024-01-01 15:09:46 +0900

    sudoで実行する

--- render_install.sh
+++ render_install.sh
@@ -1,4 +1,4 @@
 #
 pip install --upgrade pip
 pip install -r requirements.txt
-apt-get install -y tesseract-ocr tesseract-ocr-jpn tesseract-ocr-eng libtesseract-dev libleptonica-dev tesseract-ocr-script-jpan tesseract-ocr-script-jpan-vert 
+sudo apt-get install -y tesseract-ocr tesseract-ocr-jpn tesseract-ocr-eng libtesseract-dev libleptonica-dev tesseract-ocr-script-jpan tesseract-ocr-script-jpan-vert 
