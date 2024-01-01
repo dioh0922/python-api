c1de82211b41780e91d31e62ea3c6173e7943c81
dioh0922 <diohound@gmail.com>
2024-01-01 15:04:45 +0900

    render用インストールバッチ追加

---
+++ render_install.sh
@@ -0,0 +1,4 @@
+#
+pip install --upgrade pip
+pip install -r requirements.txt
+apt-get install -y tesseract-ocr tesseract-ocr-jpn tesseract-ocr-eng libtesseract-dev libleptonica-dev tesseract-ocr-script-jpan tesseract-ocr-script-jpan-vert 
