c70ef2b3a75d6817f892f95abf1edff5ae9b85bd
dioh0922 <diohound@gmail.com>
2024-01-02 17:25:44 +0900

    不要ソース削除

--- render_install.sh
+++
@@ -1,5 +0,0 @@
-#
-pip install --upgrade pip
-pip install -r requirements.txt
-sudo apt-get install -y tesseract-ocr tesseract-ocr-jpn tesseract-ocr-eng libtesseract-dev libleptonica-dev tesseract-ocr-script-jpan tesseract-ocr-script-jpan-vert 
-sudo cp ./tessdata/jpn.traineddata /usr/share/tesseract-ocr/5/tessdata/jpn.traineddata
